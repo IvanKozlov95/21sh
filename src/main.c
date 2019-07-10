f31515ae18d70e2a03999ab979c3d0fc506bb204
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-07-10 08:33:51 -0700

    Add initial logic: promt printing, input

--- src/main.c
+++ src/main.c
@@ -6,13 +6,33 @@
 /*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/08 23:41:01 by ivankozlov        #+#    #+#             */
-/*   Updated: 2019/07/08 23:42:47 by ivankozlov       ###   ########.fr       */
+/*   Updated: 2019/07/10 08:31:42 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "21sh.h"
+#include "dstring.h"
+
+static char		*get_input(void)
+{
+	t_string	*s;
+	char		buf[2];
+	int			ret;
+
+	buf[1] = 0;
+	s = string_init(0);
+	while ((ret = read(0, buf, 1)) && buf[0] != '\n')
+		string_append(s, buf);
+	return (string_destroy(s, TRUE));
+}
 
 int		main(void)
 {
-	ft_printf("Hello World\n");
+	// init_termconfig();
+	init_signal_handlers();
+	while (1)
+	{
+		display_prompt();
+		get_input();
+	}
 }
