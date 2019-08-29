61c15db06cf46b9497984e731b6b909a1901fa86
Ivan Kozlov <ikozlov@student.42.us.org>
2019-08-28 18:39:07 -0700

    Add init for commad line

--- src/main.c
+++ src/main.c
@@ -6,20 +6,22 @@
 /*   By: batman <ikozlov@student.42.us.org>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/08 23:41:01 by ivankozlov        #+#    #+#             */
-/*   Updated: 2019/08/22 21:07:40 by batman           ###   ########.fr       */
+/*   Updated: 2019/08/28 14:03:29 by batman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "21sh.h"
+#include "command_line.h"
 
 int		main(void)
 {
-	// init_termconfig();
 	init_env();
+	init_termconfig();
 	init_signal_handlers();
 	while (1)
 	{
 		display_prompt();
+		init_command_line();
 		handle_input();
 	}
 }
