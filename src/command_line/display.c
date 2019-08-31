b177ed8f7bfd891fd31404c43a5357a276aab657
Ivan Kozlov <ikozlov@student.42.us.org>
2019-08-31 11:39:36 -0700

    Command line refactor: remove input parameter

--- src/command_line/display.c
+++ src/command_line/display.c
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/26 22:23:06 by batman            #+#    #+#             */
-/*   Updated: 2019/08/31 06:41:28 by ikozlov          ###   ########.fr       */
+/*   Updated: 2019/08/31 11:01:41 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,6 @@
 
 t_command_line		g_command_line;
 
-void		display_command_line(char *input)
-{
-	ft_printf("%s", input + g_command_line.cursor_pos.x);
-}
-
 void		clear_current_command(void)
 {
 	string_destroy(g_command_line.cmd, false);
@@ -30,7 +25,7 @@ void		clear_current_command(void)
 	get_cursor_position(&g_command_line.cursor_pos);
 }
 
-void		display_current_command(char *input)
+void		display_current_command(void)
 {
 	tputs(tgetstr("cb", NULL), g_termconf.descriptor, ft_putc);
 	tputs(tgetstr("ce", NULL), g_termconf.descriptor, ft_putc);
@@ -38,7 +33,7 @@ void		display_current_command(char *input)
 		0, g_command_line.cursor_pos.y - 1),
 		g_termconf.descriptor, ft_putc);
 	display_prompt();
-	ft_printf("%s", input);
+	ft_printf("%s", g_command_line.cmd->content);
 	tputs(tgoto(g_termconf.cm,
 		g_command_line.cursor_pos.x - 1, g_command_line.cursor_pos.y - 1),
 		g_termconf.descriptor, ft_putc);
