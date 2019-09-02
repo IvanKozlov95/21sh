4d312482724af735ac2dcf9c2eb627fe13f2b17a
Ivan Kozlov <ikozlov@student.42.us.org>
2019-09-01 18:21:09 -0700

    Remove debug for home button

--- src/keys/home.c
+++ src/keys/home.c
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/31 06:41:32 by ikozlov           #+#    #+#             */
-/*   Updated: 2019/08/31 06:57:31 by ikozlov          ###   ########.fr       */
+/*   Updated: 2019/09/01 18:20:48 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ bool		handle_home_key(int key)
 	if (key == KHOME)
 	{
 		move_cursor_home();
-		debug("home button pressed\n");
 		return (true);
 	}
 	return (false);
